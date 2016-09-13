4d4dc3f6d0265d86f1d8c8e6736a4b00da53770d
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-09-13 10:31:59 +0300

    tests: fixed the test application

--- src/test/java/jatoo/app/test/TestApp.java
+++ src/test/java/jatoo/app/test/TestApp.java
@@ -17,21 +17,90 @@
 
 package jatoo.app.test;
 
+import java.awt.Insets;
+import java.io.File;
+
+import javax.swing.JComponent;
 import javax.swing.UIManager;
+
+import jatoo.app.App;
 
 /**
  * Test App with {@link #main(String[])} method.
  * 
  * @author Cristian Sulea ( http://cristian.sulea.net )
- * @version 2.0, May 19, 2014
+ * @version 3.0, September 13, 2016
  */
-public class TestApp {
+public class TestApp extends App {
+
+  static final File WORKING_DIRECTORY = new File("target");
+  static final String LOOK_AND_FEEL = UIManager.getSystemLookAndFeelClassName();
 
   public static void main(String[] args) throws Throwable {
-
-    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-
-//    App.launch(false, TestAppContentPane.class);
+    UIManager.setLookAndFeel(LOOK_AND_FEEL);
+    new TestApp();
   }
+
+  public TestApp() {
+    super("Test App");
+  }
+
+  @Override
+  protected void init() {
+
+  }
+
+  @Override
+  protected JComponent getContentPane() {
+    return new TestAppContentPane(this);
+  }
+
+  @Override
+  protected final File getWorkingDirectory() {
+    return WORKING_DIRECTORY;
+  }
+
+  @Override
+  protected final boolean isDialog() {
+    return false;
+  }
+
+  @Override
+  protected final boolean isUndecorated() {
+    return false;
+  }
+
+  @Override
+  protected final boolean isResizable() {
+    return true;
+  }
+
+  @Override
+  protected boolean isSizePersistent() {
+    return false;
+  }
+
+  @Override
+  protected final boolean hasTrayIcon() {
+    return false;
+  }
+
+  @Override
+  protected final boolean isGlueToMarginsOnCtrlArrows() {
+    return false;
+  }
+
+  @Override
+  protected final Insets getMarginsGlueGaps() {
+    return null;
+  }
+
+  @Override
+  protected final JComponent getDragToMoveComponent() {
+    return null;
+  }
+
+  @Override
+  protected final void afterInit() {}
 
 }
--- src/test/java/jatoo/app/test/TestAppContentPane.java
+++ src/test/java/jatoo/app/test/TestAppContentPane.java
@@ -17,65 +17,78 @@
 
 package jatoo.app.test;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import jatoo.app.App;
 
 /**
  * Test App Content Pane.
  * 
  * @author Cristian Sulea ( http://cristian.sulea.net )
- * @version 2.1, May 21, 2014
+ * @version 2.2, September 13, 2016
  */
-public class TestAppContentPane {
+@SuppressWarnings("serial")
+public class TestAppContentPane extends JPanel {
 
-//	@Override
-//	protected void init(final App app) {
-//
-//		app.getLogger().debug("debug");
-//		app.getLogger().info("info");
-//		app.getLogger().warn("warn");
-//		app.getLogger().error("error");
-//		app.getLogger().fatal("fatal");
-//
-//		JButton showTrayMessage = new JButton("Show Tray Message");
-//		showTrayMessage.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				app.showTrayMessage("1 ncerc sa configurez un system de test pentru Migros. POS", "1 mesPOS-ul face restart la linia din system.log:\n�lisysld: System Shutdown requested by 00000 Loc 011�sage");
-//			}
-//		});
-//
-//		JButton showTrayInformationMessage = new JButton("Show Information Tray Message");
-//		showTrayInformationMessage.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				app.showTrayInformationMessage("22 ncerc sa configurez un system de test pentru Migros.", "22 mesPOS-ul face restart la .log:\n�lisysld: requested by 00000 Loc 011�sage");
-//			}
-//		});
-//
-//		JButton showTrayWarningMessage = new JButton("Show Warning Tray Message");
-//		showTrayWarningMessage.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				app.showTrayWarningMessage("333 ncerc sa configurez un system de test pentru Migros. POS", "333 mesPOS-ul face restart la linia din system.log:\n�lisysld: System Shutdown requested by 00000 Loc 011�sage");
-//			}
-//		});
-//
-//		JButton showTrayErrorMessage = new JButton("Show Error Tray Message");
-//		showTrayErrorMessage.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				app.showTrayErrorMessage("4444 ncerc sa configurez un system de test pentru Migros.", "4444 mesPOS-ul face restart la .log:\n�lisysld: requested by 00000 Loc 011�sage");
-//			}
-//		});
-//
-//		add(showTrayMessage);
-//		add(showTrayInformationMessage);
-//		add(showTrayWarningMessage);
-//		add(showTrayErrorMessage);
-//		
-//    JButton pack = new JButton("pack()");
-//    pack.addActionListener(new ActionListener() {
-//      public void actionPerformed(ActionEvent e) {
-//        app.getWindow().pack();
-//      }
-//    });
-//    
-//    add(pack);
-//	}
+  public TestAppContentPane(final App app) {
+
+    app.getLogger().debug("debug");
+    app.getLogger().info("info");
+    app.getLogger().warn("warn");
+    app.getLogger().error("error");
+    app.getLogger().fatal("fatal");
+
+    JButton showTrayMessage = new JButton("Show Tray Message");
+    showTrayMessage.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        
+        app.showMessage("xxx");
+        app.showMessages(Arrays.asList("xxx", "yyy"));
+        
+//        app.showTrayMessage("1 ncerc sa configurez un system de test pentru Migros. POS", "1 mesPOS-ul face restart la linia din system.log:" + System.getProperty("line.separator") + "xxx �lisysld: System Shutdown requested by 00000 Loc 011�sage");
+//        app.showTrayMessage("1 mesPOS-ul face restart la linia din system.log:" + System.getProperty("line.separator") + "xxx �lisysld: System Shutdown requested by 00000 Loc 011�sage");
+      }
+    });
+
+    JButton showTrayInformationMessage = new JButton("Show Information Tray Message");
+    showTrayInformationMessage.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        app.showTrayInformationMessage("22 ncerc sa configurez un system de test pentru Migros.", "22 mesPOS-ul face restart la .log:\n�lisysld: requested by 00000 Loc 011�sage");
+      }
+    });
+
+    JButton showTrayWarningMessage = new JButton("Show Warning Tray Message");
+    showTrayWarningMessage.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        app.showTrayWarningMessage("333 ncerc sa configurez un system de test pentru Migros. POS", "333 mesPOS-ul face restart la linia din system.log:\n�lisysld: System Shutdown requested by 00000 Loc 011�sage");
+      }
+    });
+
+    JButton showTrayErrorMessage = new JButton("Show Error Tray Message");
+    showTrayErrorMessage.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        app.showTrayErrorMessage("4444 ncerc sa configurez un system de test pentru Migros.", "4444 mesPOS-ul face restart la .log:\n�lisysld: requested by 00000 Loc 011�sage");
+      }
+    });
+
+    add(showTrayMessage);
+    add(showTrayInformationMessage);
+    add(showTrayWarningMessage);
+    add(showTrayErrorMessage);
+
+    JButton pack = new JButton("pack()");
+    pack.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        app.getWindow().pack();
+      }
+    });
+
+    add(pack);
+  }
 
 }
